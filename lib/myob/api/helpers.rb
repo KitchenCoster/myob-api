c2833ad1d8a2b532a33c032c267c109be2694025
samhow <samh@oakfocus.net>
2017-01-23 22:22:49 +1100

    Removed refinement from helper

--- lib/myob/api/helpers.rb
+++ lib/myob/api/helpers.rb
@@ -1,23 +1,13 @@
 module Myob
   module Api
+    module Helpers
 
-    module RefinedString
-      refine String do
-        def underscore
-          self.gsub(/::/, '/').
+      def model(model_name)
+        method_name = model_name.to_s.gsub(/::/, '/').
           gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
           gsub(/([a-z\d])([A-Z])/,'\1_\2').
           tr("-", "_").
           downcase
-        end
-      end
-    end
-
-    module Helpers
-      using RefinedString
-
-      def model(model_name)
-        method_name = model_name.to_s.underscore
         variable_name = "@#{method_name}_model".to_sym
         unless instance_variable_defined?(variable_name)
           instance_variable_set(variable_name, Myob::Api::Model.const_get("#{model_name}".to_sym).new(self, model_name.to_s))
