9c8d5cd81f103bc7f918dc3e9c15bfc08bb3eade
samhowley <samh@oakfocus.net>
2017-02-07 02:17:42 +1100

    Create order_miscellaneous.rb

---
+++ lib/myob/api/models/order_miscellaneous.rb
@@ -0,0 +1,11 @@
+module Myob
+  module Api
+    module Model
+      class OrderItem < Base
+        def model_route
+          'Purchase/Order/Miscellaneous'
+        end
+      end
+    end
+  end
+end
