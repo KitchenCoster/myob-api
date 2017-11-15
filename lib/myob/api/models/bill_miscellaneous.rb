bb7a5895295768c9dc33547f9f53dd010dc19400
samhowley <samh@oakfocus.net>
2017-11-15 22:07:32 +1100

    Create bill_miscellaneous.rb

---
+++ lib/myob/api/models/bill_miscellaneous.rb
@@ -0,0 +1,11 @@
+module Myob
+  module Api
+    module Model
+      class BillMiscellaneous < Base
+        def model_route
+          'Purchase/Bill/Miscellaneous'
+        end
+      end
+    end
+  end
+end
